470dd1d0e25816bc1288dfd5759b9edcb8ce78fe
Bilgihan Kose <bilgihankose@gmail.com>
2022-02-04 10:51:59 +0300

    added style

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -2,10 +2,10 @@
 <div class="d-flex product-container mb-2 p-3 border">
   <img :src="imageURL" alt="product image" width="100px" height="300px">
   <div class="p-3">
-    <h5 class="card-title">{{ product.brand}}</h5>
+    <h4 class="card-title text-primary">{{ product.brand}}</h4>
     <p>{{ product.name }}</p>
-    <p >{{ product.priceText }}</p>
     <p class="text-muted">{{ product.categories }}</p>
+    <h5 class="font-weight-bolder price-tag">{{ product.priceText }}</h5>
     <button @click="$emit('add-to-cart',product)" class="btn btn-outline-primary add-to-card">Add to cart</button>
   </div>
 </div>
@@ -28,5 +28,13 @@ export default {
   position: absolute;
   bottom: 25px;
   right: 30px;
+}
+.price-tag{
+  position: absolute;
+  bottom: 60px;
+  right: 30px;
+}
+.product-container:hover {
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
 }
 </style>
